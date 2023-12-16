9207c1de0473b76e453b2a1d7eb357df3f3f36a0
OguKeith123 <keithogu1@gmail.com>
2023-12-16 01:00:25 -0500

    Initial commit

---
+++ GitHub Project Redo (RNG).py
@@ -0,0 +1,16 @@
+import random
+
+Random_Number = random.randint(0,100000)
+
+print(Random_Number)
+
+# Output: A random number between 0 and 100,000 is generated and provided
+
+
+import random
+
+Z = random.randint(500,50000)
+
+print(Z)
+
+# Output: A random number between 500 and 50,000 is produced
